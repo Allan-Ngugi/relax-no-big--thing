caf9bc449c23eee553e8ea2d9eb723b18841dfd9
Allan-Ngugi <blessedallan1@gmail.com>
2023-12-25 22:41:37 +0300

    ✨  death-reg -form

---
+++ deathreg/src/lib/components/death-reg-form/death-reg-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'reg-angular-death-reg-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './death-reg-form.component.html',
+  styleUrl: './death-reg-form.component.scss',
+})
+export class DeathRegFormComponent {}
